a1d52e229cd7bf8ce1300d02390ffb723143b5a6
Karthikeyan Gopal <karthikeyangopal@Karthikeyans-Mac-mini.local>
2021-04-17 18:14:30 +0530

    17-4-21

--- lib/Screens/Report.dart
+++ lib/Screens/Report.dart
@@ -1,14 +1,15 @@
 
 import 'dart:convert';
 import 'dart:io';
-
+import 'package:flutter/services.dart';
+import 'package:syncfusion_flutter_xlsio/xlsio.dart' hide Column, Alignment, Row;
 import 'package:anandhasapp/Models/ReportsModel.dart';
 import 'package:anandhasapp/Models/ResponseModelList1.dart';
 import 'package:anandhasapp/Screens/LoginPage.dart';
 import 'package:anandhasapp/String_Values.dart';
 import 'package:connectivity/connectivity.dart';
-import 'package:excel/excel.dart';
-
+// import 'package:excel/excel.dart';
+import 'package:open_file/open_file.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:path/path.dart';
@@ -63,7 +64,232 @@ class _ReportsState extends State<Reports> {
 
   ReportsModelList li12;
 
-  Excel excel;
+  Directory tempDir;
+
+  Future<void> generateExcel() async {
+    //Create a Excel document.
+
+    //Creating a workbook.
+    final Workbook workbook = Workbook();
+    //Accessing via index
+    final Worksheet sheet = workbook.worksheets[0];
+    sheet.showGridlines = false;
+
+
+
+
+
+// Save the document.
+//     final List<int> byte = workbook.saveAsStream();
+    // Enable calculation for worksheet.
+    sheet.enableSheetCalculations();
+// sheet.getRangeByName("B2").setValue(picture);
+    //Set data in the worksheet.
+    sheet.getRangeByName('A1').columnWidth = 10;
+    sheet.getRangeByName('B1').columnWidth = 10;
+    sheet.getRangeByName('C1').columnWidth = 10;
+    sheet.getRangeByName('D1').columnWidth = 18;
+    sheet.getRangeByName('E1').columnWidth = 10;
+    sheet.getRangeByName('F1').columnWidth = 10;
+    sheet.getRangeByName('G1').columnWidth = 10;
+    sheet.getRangeByName('H1').columnWidth = 10;
+
+    sheet.getRangeByName('A1:H1').cellStyle.backColor = '#333F4F';
+    sheet.getRangeByName('A1:H1').merge();
+    sheet.getRangeByName('B4:D6').merge();
+
+    sheet.getRangeByName('B4').setText('Sales Report');
+    sheet.getRangeByName('B4').cellStyle.fontSize = 24;
+
+    // sheet.getRangeByName('B8').setText('BILL TO:');
+    // sheet.getRangeByName('B8').cellStyle.fontSize = 9;
+    // sheet.getRangeByName('B8').cellStyle.bold = true;
+    //
+    // sheet.getRangeByName('B9').setText('Abraham Swearegin');
+    // sheet.getRangeByName('B9').cellStyle.fontSize = 12;
+    //
+    // sheet
+    //     .getRangeByName('B10')
+    //     .setText('United States, California, San Mateo,');
+    // sheet.getRangeByName('B10').cellStyle.fontSize = 9;
+    //
+    // sheet.getRangeByName('B11').setText('9920 BridgePointe Parkway,');
+    // sheet.getRangeByName('B11').cellStyle.fontSize = 9;
+    //
+    // sheet.getRangeByName('B12').setNumber(9365550136);
+    // sheet.getRangeByName('B12').cellStyle.fontSize = 9;
+    // sheet.getRangeByName('B12').cellStyle.hAlign = HAlignType.left;
+    //
+    // final Range range1 = sheet.getRangeByName('F8:G8');
+    // final Range range2 = sheet.getRangeByName('F9:G9');
+    // final Range range3 = sheet.getRangeByName('F10:G10');
+    // final Range range4 = sheet.getRangeByName('F11:G11');
+    // final Range range5 = sheet.getRangeByName('F12:G12');
+    //
+    // range1.merge();
+    // range2.merge();
+    // range3.merge();
+    // range4.merge();
+    // range5.merge();
+
+    // sheet.getRangeByName('F8').setText('INVOICE#');
+    // range1.cellStyle.fontSize = 8;
+    // range1.cellStyle.bold = true;
+    // range1.cellStyle.hAlign = HAlignType.right;
+    //
+    // sheet.getRangeByName('F9').setNumber(2058557939);
+    // range2.cellStyle.fontSize = 9;
+    // range2.cellStyle.hAlign = HAlignType.right;
+    //
+    // sheet.getRangeByName('F10').setText('DATE');
+    // range3.cellStyle.fontSize = 8;
+    // range3.cellStyle.bold = true;
+    // range3.cellStyle.hAlign = HAlignType.right;
+    //
+    // sheet.getRangeByName('F11').dateTime = DateTime(2020, 08, 31);
+    // sheet.getRangeByName('F11').numberFormat =
+    // '[\$-x-sysdate]dddd, mmmm dd, yyyy';
+    // range4.cellStyle.fontSize = 9;
+    // range4.cellStyle.hAlign = HAlignType.right;
+    //
+    // range5.cellStyle.fontSize = 8;
+    // range5.cellStyle.bold = true;
+    // range5.cellStyle.hAlign = HAlignType.right;
+    //
+    // final Range range6 = sheet.getRangeByName('B15:G15');
+    // range6.cellStyle.fontSize = 10;
+    // range6.cellStyle.bold = true;
+    //
+    sheet.getRangeByIndex(8, 2).setText('Order No');
+    for(int i=0;i<li6.details.length;i++)
+    sheet.getRangeByIndex(9+i, 2).setText(li6.details[i].orderNo);
+
+    sheet.getRangeByIndex(8, 3).setText('Name');
+    for(int i=0;i<li6.details.length;i++)
+      sheet.getRangeByIndex(9+i, 3).setText(li6.details[i].name);
+
+    sheet.getRangeByIndex(8, 4).setText('Bill Date');
+    for(int i=0;i<li6.details.length;i++)
+      sheet.getRangeByIndex(9+i, 4).setText("${DateFormat("hh:mm a, dd-MM-yyyy").format(DateTime.fromMillisecondsSinceEpoch(int.parse(li6.details[i].docDate.toString().replaceAll("/Date(", "").replaceAll(")/", ""))))}");
+
+    sheet.getRangeByIndex(8, 5).setText('Bill Amount');
+    for(int i=0;i<li6.details.length;i++)
+      sheet.getRangeByIndex(9+i, 5).setText(li6.details[i].orderPrice.toString());
+
+    sheet.getRangeByIndex(8, 6).setText('Advance');
+    for(int i=0;i<li6.details.length;i++)
+      sheet.getRangeByIndex(9+i, 6).setText(li6.details[i].advanceAmount.toString());
+
+    sheet.getRangeByIndex(8, 7).setText('Discount');
+    for(int i=0;i<li6.details.length;i++)
+      sheet.getRangeByIndex(9+i, 7).setText(li6.details[i].disAmount.toString());
+
+    sheet.getRangeByIndex(8, 8).setText('Balance Receivable');
+    for(int i=0;i<li6.details.length;i++)
+      sheet.getRangeByIndex(9+i, 8).setText("${li6.details[i].orderPrice-li6.details[i].advanceAmount-li6.details[i].disAmount}");
+
+    // sheet.getRangeByIndex(16, 2).setText('CA-1098');
+    // sheet.getRangeByIndex(17, 2).setText('LJ-0192');
+    // sheet.getRangeByIndex(18, 2).setText('So-B909-M');
+    // sheet.getRangeByIndex(19, 2).setText('FK-5136');
+    // // sheet.getRangeByIndex(20, 2).setText('HL-U509');
+    //
+    // sheet.getRangeByIndex(15, 3).setText('Description');
+    // sheet.getRangeByIndex(16, 3).setText('AWC Logo Cap');
+    // sheet.getRangeByIndex(17, 3).setText('Long-Sleeve Logo Jersey, M');
+    // sheet.getRangeByIndex(18, 3).setText('Mountain Bike Socks, M');
+    // sheet.getRangeByIndex(19, 3).setText('ML Fork');
+    // sheet.getRangeByIndex(20, 3).setText('Sports-100 Helmet, Black');
+    //
+    // sheet.getRangeByIndex(15, 3, 15, 4).merge();
+    // sheet.getRangeByIndex(16, 3, 16, 4).merge();
+    // sheet.getRangeByIndex(17, 3, 17, 4).merge();
+    // sheet.getRangeByIndex(18, 3, 18, 4).merge();
+    // sheet.getRangeByIndex(19, 3, 19, 4).merge();
+    // sheet.getRangeByIndex(20, 3, 20, 4).merge();
+    //
+    // sheet.getRangeByIndex(15, 5).setText('Quantity');
+    // sheet.getRangeByIndex(16, 5).setNumber(2);
+    // sheet.getRangeByIndex(17, 5).setNumber(3);
+    // sheet.getRangeByIndex(18, 5).setNumber(2);
+    // sheet.getRangeByIndex(19, 5).setNumber(6);
+    // sheet.getRangeByIndex(20, 5).setNumber(1);
+    //
+    // sheet.getRangeByIndex(15, 6).setText('Price');
+    // sheet.getRangeByIndex(16, 6).setNumber(8.99);
+    // sheet.getRangeByIndex(17, 6).setNumber(49.99);
+    // sheet.getRangeByIndex(18, 6).setNumber(9.50);
+    // sheet.getRangeByIndex(19, 6).setNumber(175.49);
+    // sheet.getRangeByIndex(20, 6).setNumber(34.99);
+    //
+    // sheet.getRangeByIndex(15, 7).setText('Total');
+    // sheet.getRangeByIndex(16, 7).setFormula('=E16*F16+(E16*F16)');
+    // sheet.getRangeByIndex(17, 7).setFormula('=E17*F17+(E17*F17)');
+    // sheet.getRangeByIndex(18, 7).setFormula('=E18*F18+(E18*F18)');
+    // sheet.getRangeByIndex(19, 7).setFormula('=E19*F19+(E19*F19)');
+    // sheet.getRangeByIndex(20, 7).setFormula('=E20*F20+(E20*F20)');
+    // sheet.getRangeByIndex(15, 6, 20, 7).numberFormat = '\$#,##0.00';
+    //
+    // sheet.getRangeByName('E15:G15').cellStyle.hAlign = HAlignType.right;
+    // sheet.getRangeByName('B15:G15').cellStyle.fontSize = 10;
+    // sheet.getRangeByName('B15:G15').cellStyle.bold = true;
+    // sheet.getRangeByName('B16:G20').cellStyle.fontSize = 9;
+    //
+    // sheet.getRangeByName('E22:G22').merge();
+    // sheet.getRangeByName('E22:G22').cellStyle.hAlign = HAlignType.right;
+    // sheet.getRangeByName('E23:G24').merge();
+    //
+    // final Range range7 = sheet.getRangeByName('E22');
+    // final Range range8 = sheet.getRangeByName('E23');
+    // range7.setText('TOTAL');
+    // range7.cellStyle.fontSize = 8;
+    // range8.setFormula('=SUM(G16:G20)');
+    // range8.numberFormat = '\$#,##0.00';
+    // range8.cellStyle.fontSize = 24;
+    // range8.cellStyle.hAlign = HAlignType.right;
+    // range8.cellStyle.bold = true;
+    //
+    // sheet.getRangeByIndex(26, 1).text =
+    // '800 Interchange Blvd, Suite 2501, Austin, TX 78721 | support@adventure-works.com';
+    // sheet.getRangeByIndex(26, 1).cellStyle.fontSize = 8;
+    //
+    // final Range range9 = sheet.getRangeByName('A26:H27');
+    // range9.cellStyle.backColor = '#ACB9CA';
+    // range9.merge();
+    // range9.cellStyle.hAlign = HAlignType.center;
+    // range9.cellStyle.vAlign = VAlignType.center;
+    ByteData data = await rootBundle.load("logo.png");
+    List<int> byt = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    // final byt = rootBundle.load("logo.png").readAsBytesSync();
+    final Picture picture = sheet.pictures.addBase64(3, 4, base64.encode(byt));
+    picture.lastRow = 7;
+    picture.lastColumn = 8;
+    picture.height=200;
+    picture.width=200;
+//
+// //Adding a picture
+//     final List<int> bytsafd = File('logo.png').readAsBytesSync();
+//     final Picture picture = sheet.pictures.addStream(1, 1, bytsafd);
+    //Save and launch the excel.
+    final List<int> bytes = workbook.saveAsStream();
+    //Dispose the document.
+    workbook.dispose();
+
+    //Get the storage folder location using path_provider package.
+    if (Platform.isAndroid) {
+      tempDir = await getExternalStorageDirectory();
+      // Android-specific code
+    } else {
+      tempDir = await getApplicationDocumentsDirectory();
+      // iOS-specific code
+    }
+    final String path = tempDir.path;
+    final File file = File('$path/output.xlsx');
+    await file.writeAsBytes(bytes).then((value) async => await OpenFile.open('$path/output.xlsx'));
+
+    //Launch the file (used open_file package)
+    ;
+  }
 
 
   Future<bool> check() async {
@@ -961,58 +1187,86 @@ SizedBox(height: height/40,),
         ),
       ),
                   floatingActionButton: FloatingActionButton.extended(onPressed: () async {
-
-                     excel = Excel.createExcel();
-                    Sheet sheetObject = excel['Sales Report'];
-
-                     CellStyle cellStyle = CellStyle(
-                       bold: true,
-                       italic: true,
-                       textWrapping: TextWrapping.WrapText,
-                       fontFamily: getFontFamily(FontFamily.Comic_Sans_MS),
-                       rotation: 0,
-                     );
-
-                     var sheet = excel['mySheet'];
-
-                     var cell = sheet.cell(CellIndex.indexByString("A1"));
-                     cell.value = "Heya How are you I am fine ok goood night";
-                     cell.cellStyle = cellStyle;
-
-                     var cell2 = sheet.cell(CellIndex.indexByString("E5"));
-                     cell2.value = "Heya How night";
-                     cell2.cellStyle = cellStyle;
-
-                     /// printing cell-type
-                     print("CellType: " + cell.cellType.toString());
-
-                     ///
-                     ///
-                     /// Iterating and changing values to desired type
-                     ///
-                     ///
-                     // for (int row = 0; row < sheet.maxRows; row++) {
-                     //   sheet.row(row).forEach((cell1) {
-                     //     if (cell1 != null) {
-                     //       cell1.value = ' My custom Value ';
-                     //     }
-                     //   });
-                     // }
-
-                     var fileBytes = excel.save();
-                     var tempDir;
-                     if (Platform.isAndroid) {
-                       tempDir = await getExternalStorageDirectory();
-                       // Android-specific code
-                     } else {
-                       tempDir = await getApplicationDocumentsDirectory();
-                       // iOS-specific code
-                     }
-                     String tempPath = tempDir.path;
-                     print("$tempPath/text.xlsx");
-                     File(join("$tempPath/text.xlsx"))
-                       ..createSync(recursive: true)
-                       ..writeAsBytesSync(fileBytes);
+                    generateExcel();
+     //                Excel excel;
+     //                 excel = Excel.createExcel();
+     //                Sheet sheetObject = excel['Sales Report'];
+     //                sheetObject.appendRow([""]);
+     //                sheetObject.appendRow([""]);
+     //                sheetObject.appendRow(["OrderNo","Name", "GST No","Bill Date","Bill Amount","Advance","Discount", "Balance Receivable"]);
+     //                for(int i=0;i<li6.details.length;i++)
+     //                 sheetObject.appendRow([li6.details[i].orderNo, li6.details[i].name, li6.details[i].invNo,"${DateFormat("hh:mm a, dd-MM-yyyy").format(DateTime.fromMillisecondsSinceEpoch(int.parse(li6.details[i].docDate.toString().replaceAll("/Date(", "").replaceAll(")/", ""))))}",li6.details[i].orderPrice,li6.details[i].advanceAmount,li6.details[i].disAmount,"${li6.details[i].orderPrice-li6.details[i].advanceAmount-li6.details[i].disAmount}"]);
+     //
+     //                CellStyle cellStyle = CellStyle(backgroundColorHex: "#1AFF1A", fontFamily : getFontFamily(FontFamily.Calibri));
+     //
+     //                cellStyle.underline = Underline.Single; // or Underline.Double
+     //
+     //
+     //                var cell = sheetObject.cell(CellIndex.indexByString("A3"));
+     //                // cell.value = 8; // dynamic values support provided;
+     //                cell.cellStyle = cellStyle;
+     //                 cell = sheetObject.cell(CellIndex.indexByString("B3"));
+     //                cell.cellStyle = cellStyle;
+     //                cell = sheetObject.cell(CellIndex.indexByString("C3"));
+     //                cell.cellStyle = cellStyle;
+     //                cell = sheetObject.cell(CellIndex.indexByString("D3"));
+     //                cell.cellStyle = cellStyle;
+     //                cell = sheetObject.cell(CellIndex.indexByString("E3"));
+     //                cell.cellStyle = cellStyle;
+     //                cell = sheetObject.cell(CellIndex.indexByString("F3"));
+     //                cell.cellStyle = cellStyle;
+     //                cell = sheetObject.cell(CellIndex.indexByString("G3"));
+     //                cell.cellStyle = cellStyle;
+     //                cell = sheetObject.cell(CellIndex.indexByString("H3"));
+     //                cell.cellStyle = cellStyle;
+     // //                 var sheet = excel['mySheet'];
+     // //
+     // //                 var cell = sheet.cell(CellIndex.indexByString("A1"));
+     // //                 cell.value = "Heya How are you I am fine ok goood night";
+     // //                 cell.cellStyle = cellStyle;
+     // //
+     // //                 var cell2 = sheet.cell(CellIndex.indexByString("E5"));
+     // //                 cell2.value = "Heya How night";
+     // //                 cell2.cellStyle = cellStyle;
+     // //                 /*
+     // // * sheetObject.appendRow(list-iterables);
+     // // * sheetObject created by calling - // Sheet sheetObject = excel['SheetName'];
+     // // * list-iterables === list of iterables
+     // // */
+     // //
+     // //
+     // //
+     // //
+     // //                 /// printing cell-type
+     // //                 print("CellType: " + cell.cellType.toString());
+     //
+     //                 ///
+     //                 ///
+     //                 /// Iterating and changing values to desired type
+     //                 ///
+     //                 ///
+     //                 // for (int row = 0; row < sheet.maxRows; row++) {
+     //                 //   sheet.row(row).forEach((cell1) {
+     //                 //     if (cell1 != null) {
+     //                 //       cell1.value = ' My custom Value ';
+     //                 //     }
+     //                 //   });
+     //                 // }
+     //
+     //                 var fileBytes = excel.save();
+     //                 var tempDir;
+     //                 if (Platform.isAndroid) {
+     //                   tempDir = await getExternalStorageDirectory();
+     //                   // Android-specific code
+     //                 } else {
+     //                   tempDir = await getApplicationDocumentsDirectory();
+     //                   // iOS-specific code
+     //                 }
+     //                 String tempPath = tempDir.path;
+     //                 print("$tempPath/text.xlsx");
+     //                 File(join("$tempPath/text.xlsx"))
+     //                   ..createSync(recursive: true)
+     //                   ..writeAsBytesSync(fileBytes);
 
     },
     icon: Icon(Icons.download_outlined),
@@ -3735,3 +3989,4 @@ SizedBox(height: height/40,),
   }
 
 }
+
